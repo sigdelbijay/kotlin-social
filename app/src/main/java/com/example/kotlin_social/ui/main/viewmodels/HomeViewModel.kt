e3953e01a44012f582a323fc1ad3a6668b6e18c6
sigdelbjay <sigdelbjay@gmail.com>
2020-12-28 16:34:28 +1100

    Adds HomeViewModel, BaseFragment as a base class for all fragements and HomeFragment which inherits from BaseFragment

---
+++ app/src/main/java/com/example/kotlin_social/ui/main/viewmodels/HomeViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.kotlin_social.ui.main.viewmodels
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.kotlin_social.data.entities.Post
+import com.example.kotlin_social.other.Event
+import com.example.kotlin_social.other.Resource
+import com.example.kotlin_social.repositories.MainRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class HomeViewModel @ViewModelInject constructor(
+        private val repository: MainRepository,
+        private val dispatcher: CoroutineDispatcher = Dispatchers.Main
+) : BasePostViewModel(repository, dispatcher){
+
+    private val _posts = MutableLiveData<Event<Resource<List<Post>>>>()
+    override val posts: LiveData<Event<Resource<List<Post>>>>
+        get() = _posts
+
+    override fun getPosts(uid: String) {
+        _posts.postValue(Event(Resource.Loading()))
+        viewModelScope.launch(dispatcher) {
+            val result = repository.getPostForFollows()
+            _posts.postValue(Event(result))
+        }
+    }
+}
